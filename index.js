3b21041d86667673f1d20da1fca456e2cf987a68
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-06-28 08:43:51 +0500

    finish html

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let body = document.querySelector("body")
+// let body = document.querySelector("body")
 
-home.addEventListener("click", () => {
-    body.style.cssText = "background-size: 100%; background-image: url(http://images.summitmedia-digital.com/esquiremagph/images/2020/09/07/junk-food-aging.jpg); background-repeat: no-repeat"
-  })
+// home.addEventListener("click", () => {
+//     body.style.cssText = "background-size: 100%; background-image: url(http://images.summitmedia-digital.com/esquiremagph/images/2020/09/07/junk-food-aging.jpg); background-repeat: no-repeat"
+//   })
