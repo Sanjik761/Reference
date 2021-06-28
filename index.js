203cbfdc9358c9086398e68652906c9efea820f1
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-06-28 10:34:13 +0500

    finished reference

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const carZoom = document.querySelectorAll(".zoom");
 carZoom.forEach((image) => {
     image.addEventListener("mousemove", (e) => {
         image.style =
-            "background-size: 1800px 1200px;" +
+            "background-size: 2000px 1400px;" +
             `background-position: ${e.offsetX - 3 * e.offsetX}px ${e.offsetY - 3 * e.offsetY
             }px;`;
     });
